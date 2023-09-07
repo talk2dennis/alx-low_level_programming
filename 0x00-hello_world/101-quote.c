d1be0224094613ce8ea14a10a3084d019357cb31
Dennis Adigwe <adigwedennis@yahoo.com>
2023-09-07 10:58:46 -0700

    update 101-quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,8 +7,8 @@
  */
 int main(void)
 {
-	char err[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	char err[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	fprintf(stderr, "%s \n", err);
+	fprintf(stderr, "%s", err);
 	return (1);
 }
