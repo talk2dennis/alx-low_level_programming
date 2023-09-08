24bd74c4c2c010195285205a5829b8fbf6823701
Dennis Adigwe <adigwedennis@yahoo.com>
2023-09-08 13:16:21 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - the entry point to my program
+ * Return: Always 0 (Success)
+ * i is an int
+ *
+ * this program will print 01 - 89 using putchar
+ */
+
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 9; i++)
+	{
+		for (j = 1; j < 10; j++)
+		{
+			putchar('0' + i);
+			putchar('0' + j);
+
+			if (i == 8 && j == 9)
+			{
+				putchar('\n');
+			}
+			else
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+	return (0);
+}
