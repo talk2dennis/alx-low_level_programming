50bce66be173c7a6339372b94503657dba992dd4
Dennis Adigwe <adigwedennis@yahoo.com>
2023-09-08 11:47:37 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - the entry point to my program
+ * Return: Always 0 (Success)
+ * i is an int
+ *
+ * this program will print 0 - 9
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar('0' + i);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+
+	return (0);
+}
