ead9dbc7d3dee2405f3ac9716cf2c7466fb2b70e
Dennis Adigwe <adigwedennis@yahoo.com>
2023-09-08 14:54:44 -0700

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ * main - the entry point to my program
+ * Return: Always 0 (Success)
+ * i is an int
+ *
+ * this program will print 01 - 89 using putchar
+ */
+
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i <= 98; i++)
+	{
+		for (j = i; j <= 99; j++)
+		{
+			putchar('0' + (i / 10));
+			putchar('0' + (i % 10));
+
+			putchar(' ');
+
+			putchar('0' + (j / 10));
+			putchar('0' + (j % 10));
+
+			if (!(i == 9 && j == 9))
+			{
+				putchar(',');
+				putchar(' ');
+			}
+				
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
