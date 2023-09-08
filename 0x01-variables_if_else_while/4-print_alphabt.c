7ea9f33b0f6fb1495faf079dfb4f2c6fcbc042b8
Dennis Adigwe <adigwedennis@yahoo.com>
2023-09-08 10:53:21 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - the entry point to my program
+ * Return: Always 0 (Success)
+ * a is a char
+ */
+
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+		if (a == 'q' || a == 'e')
+			continue;
+		putchar(a);
+	putchar('\n');
+
+	return (0);
+}
