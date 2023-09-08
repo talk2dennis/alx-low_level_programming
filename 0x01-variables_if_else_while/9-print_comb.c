067040a3d33fac85a2de3ec13ef17cf54a97697c
Dennis Adigwe <adigwedennis@yahoo.com>
2023-09-08 11:51:38 -0700

    update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -15,8 +15,12 @@ int main(void)
 	for (i = 0; i < 10; i++)
 	{
 		putchar('0' + i);
-		putchar(',');
-		putchar(' ');
+
+		if (i < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 	putchar('\n');
 
