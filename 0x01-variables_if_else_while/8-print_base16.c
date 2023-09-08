dce0334709fec287ea7271d35da9e058be1dad90
Dennis Adigwe <adigwedennis@yahoo.com>
2023-09-08 11:40:39 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - the entry point to my program
+ * Return: Always 0 (Success)
+ * i is an int
+ *
+ * This program will print all the nubers of base 16
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 16; i++)
+	{
+		if (i < 10)
+		{
+			putchar('0' + i);
+		}
+		else
+		{
+			putchar('A' + i - 10);
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
