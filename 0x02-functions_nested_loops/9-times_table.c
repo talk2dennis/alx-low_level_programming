3cdd7e81254d9c98f7fe4519e8fd4fdf010e5717
Adigwe Dennis <63774376+talk2dennis@users.noreply.github.com>
2023-09-13 00:54:24 +0100

    Update 9-times_table.c new

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -27,7 +27,7 @@ void times_table(void)
                     _putchar(' ');
                     _putchar(' ');
                 } else {
-                    putchar(' ');
+                    _putchar(' ');
                 }
             } else {
                 _putchar('\n');
