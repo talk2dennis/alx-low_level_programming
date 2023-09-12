ef76020cf0ef892e0c513d91fb82eb4554d86bcd
Dennis Adigwe <adigwedennis@yahoo.com>
2023-09-12 15:08:39 -0700

    update print_last_digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -13,16 +13,16 @@ int print_last_digit(int n)
 	if (n == -2147483648)
 	{
 		_putchar('0' + (unsigned int) n % 10);
-		return (unsigned int) n;
+		return (unsigned int) n % 10;
 	}
 	else if (n < 0)
 	{
 		_putchar('0' + (-(n) % 10));
-		return -n;
+		return -n % 10;
 	}
 	else
 	{
 		_putchar('0' + (n % 10));
-		return (n);
+		return (n % 10);
 	}
 }
