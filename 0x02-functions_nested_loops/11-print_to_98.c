17fc999ba40e1eb68dd100620d18876b624ea05f
Adigwe Dennis <63774376+talk2dennis@users.noreply.github.com>
2023-09-13 12:25:39 +0100

    Update 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -20,7 +20,7 @@ void print_to_98(int n)
 				_putchar('0' + i % 10);
 				_putchar('\n');
 			}
-			else if (i > 9)
+			else if (i > 9 || i < 9)
 			{
 				_putchar('0' + i / 10);
 				_putchar('0' + i % 10);
