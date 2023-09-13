3372ee673ef8a0b516f597b2eb87bd74e2667466
Dennis Adigwe <adigwedennis@yahoo.com>
2023-09-13 01:39:42 -0700

    checked for the next product in times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,13 +8,14 @@
 
 void times_table(void)
 {
-	int i, j, result;
+	int i, j, result, next;
 
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j < 10; j++)
 		{
 			result = i * j;
+			next = i * (j + 1);
 			if (result >= 10)
 			{
 				_putchar('0' + (result / 10));
@@ -28,7 +29,7 @@ void times_table(void)
 			if (j < 9)
 			{
 				_putchar(',');
-				if (result < 10)
+				if (next < 10)
 				{
 					_putchar(' ');
 					_putchar(' ');
