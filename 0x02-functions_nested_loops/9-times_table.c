d72c447f342ad4b0eb4d6e17fca5cd2c8a7d23d5
Dennis Adigwe <adigwedennis@yahoo.com>
2023-09-13 01:19:23 -0700

    updated times_tables

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,30 +8,40 @@
 
 void times_table(void)
 {
-  int i, j, result;
-  for(i = 0; i < 10; i++) {
-        for(j = 0; j < 10; j++) {
-            result = i * j;
-            if (result >= 10) {
-                _putchar('0' + (result / 10));
-                _putchar('0' + (result % 10));
-            }
-            else
-            {
-            _putchar('0' + result);
-            }
-            
-            if (j < 9) {
-                _putchar(',');
-                if (result < 10) {
-                    _putchar(' ');
-                    _putchar(' ');
-                } else {
-                    _putchar(' ');
-                }
-            } else {
-                _putchar('\n');
-            }
-        }
-    }
+	int i, j, result;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			result = i * j;
+			if (result >= 10)
+			{
+				_putchar('0' + (result / 10));
+				_putchar('0' + (result % 10));
+			}
+			else
+			{
+				_putchar('0' + result);
+			}
+
+			if (j < 9)
+			{
+				_putchar(',');
+				if (result < 10)
+				{
+					_putchar(' ');
+					_putchar(' ');
+				}
+				else
+				{
+					_putchar(' ');
+				}
+			}
+			else
+			{
+				_putchar('\n');
+			}
+		}
+	}
 }
