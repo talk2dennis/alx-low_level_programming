ea1c4175c95026ba4a3cc0e9b76a2c270b54d529
Dennis Adigwe <adigwedennis@yahoo.com>
2023-09-13 23:23:31 -0700

    main.h and main.c

---
+++ 0x03-debugging/README.md
@@ -0,0 +1,3 @@
+# 0x03-debugging
+- This project is all about debugging in c 
+- There are 4 projects in this directory
---
+++ 0x03-debugging/main.c
@@ -0,0 +1,18 @@
+/**
+ * main - tests function that prints if integer is positive or negative
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 98;
+	positive_or_negative(i);
+	i = 0;
+	positive_or_negative(i);
+	i = -60;
+	positive_or_negative(i0);
+
+	return (0);
+}
