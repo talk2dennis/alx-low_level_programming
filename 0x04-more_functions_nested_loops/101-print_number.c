878f876af9349ca1bbb9e1b195bb93618435c805
Dennis Adigwe <adigwedennis@yahoo.com>
2023-09-14 16:00:51 -0700

    create 101-print_numbers

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,87 @@
+#include "main.h"
+
+/**
+ * print_number - A void print_number(int n);
+ * @n: interger to print
+ * Return: always return void
+ */
+
+void print1000(int n);
+void print100(int n);
+void print10(int n);
+
+
+void print_number(int n)
+{
+	if (n > 999)
+	{
+		print1000(n);
+	}
+	else if (n > 99)
+		print100(n);
+	else if (n > 9)
+		print10(n);
+	else
+		_putchar('0' + n);
+	if (n < -999)
+	{
+		_putchar('-');
+		print1000(-n);
+	}
+	else if (n < -99)
+	{
+		_putchar('-');
+		print100(-n);
+	}
+	else if (n < -9)
+	{
+		_putchar('-');
+		print10(-n);
+	}
+	else
+	{
+		_putchar('-');
+		_putchar('0' + -n);
+	}
+}
+
+/**
+ * print1000 - A function to print 1000 and above
+ * @n: integer to print
+ * Return: void
+ */
+
+void print1000(int n)
+{
+	_putchar('0' + n / 1000);
+	_putchar('0' + (n % 1000) / 100);
+	_putchar('0' + (n % 100) / 10);
+	_putchar('0' + (n % 10);
+}
+
+/**
+ * print100 - A function to print 100+ digits
+ * @n: integer to print
+ * Return: void
+ */
+
+void print100(int n)
+{
+	_putchar('0' + (n / 100));
+	_putchar('0' + ((n % 100) / 10));
+	_putchar('0' + (n % 10));
+}
+
+/**
+ * print10 - A function to print 10+
+ * @n: integer to print
+ * Return: void
+ */
+
+void print10(int n)
+{
+	_putchar('0' + n / 10);
+	_putchar('0' + n % 10);
+}
+
+
