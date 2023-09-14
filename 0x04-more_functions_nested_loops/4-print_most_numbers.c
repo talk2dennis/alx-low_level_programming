eda7c20aca39fe9a7c50dc2ceb144b15b307ccdf
Dennis Adigwe <adigwedennis@yahoo.com>
2023-09-14 14:03:50 -0700

    update main.h and create 4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - A void print_most_numbers(void);
+ * Return: always return void
+ *
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+	for (i = 0; i < 10; i++)
+	{
+		if (i == 2 || i == 4)
+			break;
+		_putchar('0' + i);
+	}
+
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 #include <stdio.h>
 
+void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
 void print_diagonal(int n);
