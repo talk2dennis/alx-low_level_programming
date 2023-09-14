eb30a7048f7fddb74e8d269a5514cf9556d94dc2
Dennis Adigwe <adigwedennis@yahoo.com>
2023-09-14 14:26:22 -0700

    updated main.h created 0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * isupper - A function that checks for uppercase character.
+ * Return: 1 is uppercase otherwise returns 0
+ * @c: integer to check on
+ */
+
+int _isupper(int c)
+{
+
+	if (c >= 65 || c <= 90)
+		return (1);
+	else
+		return (0);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 #include <stdio.h>
 
+int _isupper(int c);
 void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
