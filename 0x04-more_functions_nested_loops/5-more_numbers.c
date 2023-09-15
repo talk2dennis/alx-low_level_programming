14b031d8437b71e059693f704769026b96073ecc
Dennis Adigwe <adigwedennis@yahoo.com>
2023-09-15 03:24:58 -0700

    perfected 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,15 +12,15 @@ void more_numbers(void)
 {
 	int i, j;
 
-	for (i = 0; i <= 10; i++)
+	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j <= 14; j++)
 		{
 			if (j > 9)
 			{
-				_putchar('0' + j % 10);
+				_putchar('0' + j / 10);
 			}
-			_putchar('0' + j);
+			_putchar('0' + j % 10);
 		}
 		_putchar('\n');
 	}
