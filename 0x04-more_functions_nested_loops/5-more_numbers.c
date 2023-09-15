b4898983c29e104797bcd3e52c3c4fa8810527f8
Dennis Adigwe <adigwedennis@yahoo.com>
2023-09-15 02:48:35 -0700

    update 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -18,11 +18,9 @@ void more_numbers(void)
 		{
 			if (j > 9)
 			{
-				_putchar('0' + j / 10);
 				_putchar('0' + j % 10);
 			}
-			else
-				_putchar('0' + j);
+			_putchar('0' + j);
 		}
 		_putchar('\n');
 	}
