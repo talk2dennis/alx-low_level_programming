ad2c5211d1a72c3a901a4ba9adccee5a14e173e5
Adigwe Dennis <63774376+talk2dennis@users.noreply.github.com>
2023-09-15 16:18:09 +0100

    Update 100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,35 +1,27 @@
-// Online C compiler to run C program online
 #include <stdio.h>
 #include <math.h>
 
+int main() {
+    long long n = 612852475143;
+    long long max = -1;
 
-int main()
-{
-     long int i, j, max;
-     
-     max = -1;
-     
-     i = 612852475143;
-     
-     while (i % 2 == 0)
-     {
-         max = 2;
-         i /= 2;
-     }
-     
-     for (i = 3; i < sqrt(i); i = i + 2)
-     {
-         if (i % j == 0)
-         {
-             max = j;
-             i = i / j;
-         }
-     }
-     if (i > 2)
-     {
-         max = i;
-         printf("%ld\n", max);
-     }
-     
-     return (0);
+    while (n % 2 == 0) {
+        max = 2;
+        n /= 2;
+    }
+
+    for (long long i = 3; i <= sqrt(n); i = i + 2) {
+        while (n % i == 0) {
+            max = i;
+            n = n / i;
+        }
+    }
+
+    if (n > 2) {
+        max = n;
+    }
+
+    printf("%lld\n", max);
+
+    return 0;
 }
