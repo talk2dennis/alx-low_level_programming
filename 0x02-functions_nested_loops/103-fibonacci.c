23c1158aaeb70cde42663ef169c949b250c7233c
Dennis Adigwe <adigwedennis@yahoo.com>
2023-09-16 00:31:09 -0700

    103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - The entrance to my program
+ * Descriptiom: A program that finds and prints the sum of th
+ * even-valued terms, followed by a new line.
+ * from 1, 2 ...
+ * Return: Always (0)
+ *
+ */
+
+int main(void)
+{
+	int n, i;
+	long a, b, next, sum = 0;
+
+	n = 50;
+	a = 1;
+	b = 2;
+
+
+	for (i = 3; i <= n; i++)
+	{
+		if ((b < 4000000) && (b % 2 == 0))
+		{
+			sum += next;
+		}
+		next = a + b;
+
+		a = b;
+		b = next;
+	}
+	printf("%d\n", sum);
+
+	return (0);
+}
