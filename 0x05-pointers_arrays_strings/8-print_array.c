3992e38f51e6fc7ab893ad34881ac3259f935895
Dennis Adigwe <adigwedennis@yahoo.com>
2023-09-18 13:36:37 -0700

    create 8-print_array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_array - A function that prints all arrays ellement
+ * followed by a new line.
+ * @a: int a
+ * @n: int n
+ * Return: void
+ *
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i];
+		if (i + 1 < n)
+			printf(", ");
+	}
+	_putchar('\n');
+}
