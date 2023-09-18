7de50e3bdb65fa015912d772e7cd90d63a46ae10
Dennis Adigwe <adigwedennis@yahoo.com>
2023-09-18 09:28:08 +0000

    created README.md 0-reset_to_98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include <main.h>
+
+/**
+ * reset_to_98 - A function that takes a pointer to an int as
+ * parameter and updates the value it points to to 98.
+ * @n: integer pointer
+ * Return: return void
+ */
+
+
+void reset_to_98(int *n)
+{
+	*n = 402;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,6 @@
+# 0x05. C - Pointers, arrays and strings
+
+0. 98 Battery st.
+> function that takes a pointer to an int as parameter and updates the value it points to to 98.
+> void reset_to_98(int *n);
+
