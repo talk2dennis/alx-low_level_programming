9f1052f4ff444ccc1d136ec1636faf205521b07e
Dennis Adigwe <adigwedennis@yahoo.com>
2023-09-18 12:09:29 -0700

    ceate 7-puts_j

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ * puts_half - A function that prints
+ * the secod half of a string, in reverse,
+ * followed by a new line.
+ * @s: char s
+ * Return: void
+ *
+ */
+
+int _strlen(char *s);
+
+void puts_half(char *str)
+{
+	int n, i;
+
+	n = _strlen(str);
+	i = n / 2;
+	while (i < n)
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
+
+/**
+ * _strlen - a function that returns the length of a string.
+ * @s: char pointer
+ * Return: always return the length of sttring
+ *
+ */
+
+int _strlen(char *s)
+{
+	int i, n;
+
+	n = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		n++;
+	}
+
+	return (n);
+}
