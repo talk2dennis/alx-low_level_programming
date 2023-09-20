755e402c3c7b6148deca0cd184dce960b0d88837
Dennis Adigwe <adigwedennis@yahoo.com>
2023-09-20 14:00:30 +0000

    create _strcmp

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -4,6 +4,8 @@
 #include <stdio.h>;
 car *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
 
 #endif
 
