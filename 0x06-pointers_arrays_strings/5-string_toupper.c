15075d18ed327fc06b56a5dd9843c5f2805c634b
Dennis Adigwe <adigwedennis@yahoo.com>
2023-09-20 12:48:42 -0700

    create 5-string_toupper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * string_toupper - a function that changes an array to upper case
+ * @a: char a
+ * Return: Return a pointer to array
+ */
+
+char *string_toupper(char *a)
+{
+	int i;
+	char *ptr;
+
+	for (i = 0; a[i] != '\0'; i++)
+	{
+		if (a[i] >= 'a' && a[i] <= 'z')
+			a[i] -= 32;
+	}
+	return (a);
+}
