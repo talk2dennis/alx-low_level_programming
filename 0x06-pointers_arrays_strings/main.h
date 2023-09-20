c60fee7b5cd8c9efcc306762004cd4a11f92b3bd
Dennis Adigwe <adigwedennis@yahoo.com>
2023-09-20 11:54:08 -0700

    update main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>;
+#include <stdio.h>
 car *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
