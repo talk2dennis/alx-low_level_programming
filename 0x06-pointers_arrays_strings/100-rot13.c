bbd7acb56a1285e329617fd86d12d0a195061b90
Dennis Adigwe <adigwedennis@yahoo.com>
2023-09-21 14:04:11 +0000

    create 100-rot13.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * rot13 - a function that encodes a string using rot13.
+ * @str: a string to encode
+ * Return: a pointer to the string encoded
+ */
+
+char *rot13(char *str)
+{
+	int i, rot = 13;
+
+	i = 0;
+	while (str[i] != 0)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] = ((str[i] - 'a' + rot) % 26) + 'a';
+		else if (str[i] >= 'A' && str[i] <= 'Z')
+			str[i] = ((str[i] - 'A' + rot) % 26) + 'A';
+		i++;
+	}
+	return (str);
+}
