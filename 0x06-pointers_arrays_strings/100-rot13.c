11ec10dd8cd9884a67413a8561c4e4983fef7186
Dennis Adigwe <adigwedennis@yahoo.com>
2023-09-22 08:54:03 +0000

    update 16 100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -13,7 +13,7 @@ char *rot13(char *str)
 	i = 0;
 	while (str[i] != 0)
 	{
-		if (str[i] >= 'a' && str[i] <= 'z')
+		if ((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z'))
 			str[i] = ((str[i] - 'a' + rot) % 26) + 'a';
 		i++;
 	}
