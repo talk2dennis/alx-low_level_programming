35764c13409ff624e74dc6b6ec6610cf25ad55d2
Dennis Adigwe <adigwedennis@yahoo.com>
2023-09-25 11:28:30 +0000

    update 1-memcpy update main.h

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -8,13 +8,13 @@
  * Return: Return a dest
  */
 
-char *_memcpy(char *dest, char src, unsigned int n)
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
 	for (i = 0; i < n; i++)
 	{
-		s[i] = src[i];
+		dest[i] = src[i];
 	}
 	return (dest);
 }
