1a3e4a337386b3a4b68948067cbcd0ba971626a1
Dennis Adigwe <adigwedennis@yahoo.com>
2023-09-25 11:22:23 +0000

    create 1-memcpy update main.h

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memcpy - a function that copies memory area from src to dest
+ * @dest: char dest
+ * @src: char src
+ * @n: int n
+ * Return: Return a dest
+ */
+
+char *_memcpy(char *dest, char src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = src[i];
+	}
+	return (dest);
+}
