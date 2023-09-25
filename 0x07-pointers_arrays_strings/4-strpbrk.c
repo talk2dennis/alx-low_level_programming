93304557da8e01c4e590961a2aa0d7657cf1dd5b
Dennis Adigwe <adigwedennis@yahoo.com>
2023-09-25 13:50:51 +0000

    create 4-strpbrk.c

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strpbrk - a function that searches a string for any of a set of bytes
+ * @s: char s
+ * @accept: char accept
+ * Return: A pointer to first occurance of c
+ *
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+
+	for (i = 0; s[i] >= '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+				return (&s[i]);
+		}
+	}
+	return (NULL);
+}
