56d2126d5b2de8eab15c1aabef4931e49023c69e
Dennis Adigwe <adigwedennis@yahoo.com>
2023-09-25 20:34:31 +0000

    update main.h create 8-print_diagsums

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -8,6 +8,7 @@ char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 void print_chessboard(char (*a)[8]);
+void print_diagsums(int *a, int size);
 
 
 #endif
