4831a8f4385a80764a4e5ef8a3694443e5cc13ed
Adigwe Dennis <63774376+talk2dennis@users.noreply.github.com>
2023-09-26 12:08:59 +0100

    Update 5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -10,18 +10,20 @@
 
 char *_strstr(char *haystack, char *needle)
 {
-	int i, j, k;
+    int i, j, k;
 
-	for (i = 0; haystack[i] != '\0'; i++)
-	{
-		j = i, k = 0;
-		while (haystack[j] == needle[k])
-		{
-			if (needle[k + 1] == ' ' || needle[k + 1] == '\0')
-				return (&haystack[i]);
-			k++;
-			j++;
-		}
-	}
-	return ("");
+   
+
+    for (i = 0; haystack[i] != '\0'; i++)
+    {
+        j = i, k = 0;
+        while (haystack[j] == needle[k])
+        {
+            j++;
+            k++;
+        }
+        if (needle[k] == '\0')
+                return (&haystack[i]); // Return starting index of match
+    }
+    return (NULL);
 }
