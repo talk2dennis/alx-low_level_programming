eb594a80a1df57cb3f77ac47ec86d77954a04a15
Dennis Adigwe <adigwedennis@yahoo.com>
2023-09-26 11:45:14 +0000

    create 100-set_string.c

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -10,6 +10,7 @@ unsigned int _strspn(char *s, char *accept);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 char *_strstr(char *haystack, char *needle);
+void set_string(char **s, char *to);
 
 
 #endif
