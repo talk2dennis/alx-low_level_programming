4cb70d621cd42b4c893af5e33f146b0dff3bb709
Dennis Adigwe <adigwedennis@yahoo.com>
2023-09-26 02:01:23 -0700

    create 5-strstr.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strstr - A function that locates a substring.
+ * @haystack: A string to search
+ * @needle: a substring
+ * Return a pointer to the string found
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j, k;
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		j = i, k = 0;;
+		while (haystack[j] == needle[k])
+		{
+			if (needle[k + 1] == ' ' || needle[k + 1] == '\0')
+				return (haystack[j]);
+			k++;
+		}
+	}
+	return (NULL);
+}
