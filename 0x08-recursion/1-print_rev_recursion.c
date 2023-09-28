a570692330e3896208da92efee0189f088b479a6
Dennis Adigwe <adigwedennis@yahoo.com>
2023-09-28 13:21:34 +0000

    create 1-print_recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - function that prints a string in reverse.
+ * @s: a string
+ * Return: nothing
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		putchar('\n');
+		return;
+	}
+	_print_rev_recursion(s + 1);
+	putchar(*s);
+}
