1cda9d51f77fa251d9c479311b9d34e34e4b37c1
Dennis Adigwe <adigwedennis@yahoo.com>
2023-09-29 10:56:41 +0000

    update2 101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -14,7 +14,7 @@ int wildcmp(char *s1, char *s2)
 	if (*s1 == '\0' && *s2 == '\0')
 		return (1);
 	if (*s1 == '*')
-		return wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2);
+		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
 	if (*s1 == *s2)
 		return (wildcmp(s1 + 1, s2 + 1));
 	return (0);
