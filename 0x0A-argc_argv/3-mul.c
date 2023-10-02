7f4f7984b77489ff8adeb496623869c9cb5a002e
Dennis Adigwe <adigwedennis@yahoo.com>
2023-10-02 10:09:40 -0700

    updated 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -14,7 +14,10 @@ int main(int argc, char *argv[])
 	int num2;
 
 	if (argc != 3)
+	{
+		printf("Error\n");
 		return (1);
+	}
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[2]);
 	printf("%d\n", num1 * num2);
