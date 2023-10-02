e392923d3f08e0ce33d012169a5b9a6961c7c0d3
Dennis Adigwe <adigwedennis@yahoo.com>
2023-10-02 09:32:35 -0700

    created 1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - function that prints out the number of argurement
+ * @argc: int argc
+ * @argv: string array argurement
+ * Return: always (0)
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argv;
+
+	printf("%d\n", argc);
+	return (0);
+}
