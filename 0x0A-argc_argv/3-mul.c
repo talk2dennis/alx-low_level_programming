0759061aae4b1fba1166acdcdecca7e6254f7ba5
Dennis Adigwe <adigwedennis@yahoo.com>
2023-10-02 10:06:06 -0700

    created 3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - function that prints out the sum of two argurement
+ * @argc: int argc
+ * @argv: string argv
+ * Return: always (0)
+ */
+
+int main(int argc, char *argv[])
+{
+	int num1;
+	int num2;
+
+	if (argc != 3)
+		return (1);
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	printf("%d\n", num1 * num2);
+	return (0);
+}
