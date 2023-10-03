ed9e7aff31998933ff4c1a8eb5f852d152b68067
Dennis Adigwe <adigwedennis@yahoo.com>
2023-10-03 08:37:10 +0000

    updated 0-create_arary.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,7 +14,7 @@ char *create_array(unsigned int size, char c)
 	unsigned int i = 0;
 
 	ptr = malloc(sizeof(char) * size);
-	if (ptr == NULL)
+	if (ptr == NULL || size == 0)
 		return (NULL);
 	while (i < size)
 	{
