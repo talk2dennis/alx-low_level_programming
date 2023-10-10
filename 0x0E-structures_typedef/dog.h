0e91e0b90f5babb872ad57717709cbc293cebc64
Dennis Adigwe <adigwedennis@yahoo.com>
2023-10-10 12:01:45 +0000

    updated betty dog.h header

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - Define a new type struct dog with the following elements
+ * @name: string name of the dog
+ * @age: float age of the dog
+ * @owner: string owners name
+ */
+
 struct dog
 {
 	char *name;
