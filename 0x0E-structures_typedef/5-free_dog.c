4d626a414031794f23e99fc00c4faef68c914d9a
Dennis Adigwe <adigwedennis@yahoo.com>
2023-10-10 13:38:49 -0700

    created 5-free_dog.v

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - function that frees dog
+ * @d: struct dog
+ * Return: void
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
