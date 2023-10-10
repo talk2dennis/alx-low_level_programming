19f81b2aa173a9df71692330380fe582cd2d05fc
Dennis Adigwe <adigwedennis@yahoo.com>
2023-10-10 14:00:29 -0700

    updated check for null input

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -46,6 +46,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *mydog;
 
+	if (name == NULL || owner == NULL)
+		return (NULL);
 	mydog = malloc(sizeof(dog_t));
 	if (mydog == NULL)
 		return (NULL);
