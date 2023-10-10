836c12ebb91870df9dbcb0c9f24605ac2a3e3719
Dennis Adigwe <adigwedennis@yahoo.com>
2023-10-10 10:23:08 +0000

    last push

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - function that prints out the name of the c file
+ * Return: always (0)
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,6 @@
+#ifndef _ABS_H
+#define _ABS_H
+
+#define ABS(x) ((x) < (0) ? (-(x)) : (x))
+
+#endif
