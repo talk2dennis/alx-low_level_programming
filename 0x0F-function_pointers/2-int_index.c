22dd434804bad136ccc5c1cb08eb5295b239518f
Dennis Adigwe <adigwedennis@yahoo.com>
2023-10-11 12:20:21 +0000

    updated 2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -16,7 +16,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 	if (array == NULL || cmp == NULL)
 		return (-1);
 	if (size <= 0)
-	       	return (-1);
+		return (-1);
 	for (i = 0; i < size; i++)
 	{
 		if (cmp(array[i]) != 0)
