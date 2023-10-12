ef85b031ceaab57d86da82e16af8fa5cd9638c29
Dennis Adigwe <adigwedennis@yahoo.com>
2023-10-12 12:41:42 +0000

    created 1-print_numbers.c

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,25 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - Write a function that prints numbere.
+ * @separator: string seperator
+ * @n: number of args
+ * Return: void
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list arg;
+
+	va_start(arg, n);
+	if (separator != NULL)
+	{
+		for (i = 0; i < n; i++)
+		{
+			printf("%d", va_arg(arg, int));
+			putchar(*separator);
+		}
+		putchar('\n');
+	}
+}
