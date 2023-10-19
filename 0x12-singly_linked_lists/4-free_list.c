4070b2b2b002613d6e2b8e1846200230dfe8512f
Dennis Adigwe <adigwedennis@yahoo.com>
2023-10-19 07:09:31 -0700

    created free_list.c

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * print_list - A function to print out all the elements of a list
+ * @h: list to print out the elements
+ * Return: the size of the list
+ */
+
+void free_list(list_t *head)
+{
+	list_t temp;
+
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp->str);
+		free(temp);
+	}
+}
