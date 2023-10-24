7ba3c96eccd240cd140c00f511f25724bd393b20
Dennis Adigwe <adigwedennis@gmail.com>
2023-10-24 20:38:39 +0100

    updated8 5-free_listint2.c for betty

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,13 +10,13 @@ void free_listint2(listint_t **head)
 {
 	listint_t *tmp;
 
-	if (*head == NULL || head == NULL)
+	if (*head == NULL)
 		return;
 	while ((*head)->next != NULL)
 	{
-		tmp = *head;
-		*head = (*head)->next;
-		free(tmp);
+		tmp = (*head)->next;
+		free(*head);
+		*head = tmp;
 	}
 	free(*head);
 	*head = NULL;
