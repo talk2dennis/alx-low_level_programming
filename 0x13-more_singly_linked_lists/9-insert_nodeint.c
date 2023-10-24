1e4c11b797294944826d788a6dc6de525fb5d9c1
Dennis Adigwe <adigwedennis@gmail.com>
2023-10-24 13:16:40 +0100

    updated2 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -21,7 +21,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	node->next = NULL;
 	if (idx == 0)
 	{
-		node = *head;
+		node->next = *head;
 		*head = node;
 		return (*head);
 	}
