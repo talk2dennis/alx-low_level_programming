87e6c8ae21e3c3dd41e9896ef27fe157dee79001
Dennis Adigwe <adigwedennis@gmail.com>
2023-10-24 20:54:07 +0100

    updated the code

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,13 +10,13 @@ void free_listint2(listint_t **head)
 {
 	listint_t *tmp;
 
-	if (*head == NULL || head == NULL)
+	if (*head == NULL)
 		return;
-	while ((*head)->next != NULL)
+	while (*head != NULL)
 	{
-		tmp = (*head)->next;
-		free(*head);
-		*head = tmp;
+		tmp = *head;
+		*head = tmp->next;
+		free(tmp);
 	}
 	free(*head);
 	*head = NULL;
