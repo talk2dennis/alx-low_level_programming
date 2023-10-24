19322aa576373abb4a3b30bc64f2f88fdb56fc7e
Dennis Adigwe <adigwedennis@gmail.com>
2023-10-24 10:36:56 +0100

    created free_listint2.c

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_listint - A function that frees listint of memory allocated
+ * @head: the linked list
+ * Return: nothing
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp;
+
+	while (*head != NULL)
+	{
+		tmp = *head;
+		*head = (*head)->next;
+		free(tmp);
+	}
+	*head = NULL;
+}
