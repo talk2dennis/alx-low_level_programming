828c67d19837d833164b1ae33a95c2cbd432d6ad
Dennis Adigwe <adigwedennis@gmail.com>
2023-10-26 16:29:41 +0100

    updated the code

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -5,6 +5,7 @@
  * @n: number to flip bits
  * @m: number of bits to flip
  * Return: Number of bits to flip
+ *
  */
 
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
@@ -12,11 +13,11 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 	unsigned int count = 0;
 	unsigned long int bit = n ^ m;
 
-	printf("%lu\n", bit);
-	while (bits > 0)
+	while (bit > 0)
 	{
 		count += bit & 1;
 		bit >>= 1;
 	}
+	
 	return (count);
 }
