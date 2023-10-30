c109c0afb2703887c0803ee2da38a1cd5af9dd5f
Dennis Adigwe <adigwedennis@gmail.com>
2023-10-30 19:35:52 +0100

    updated create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,7 +17,7 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_WRONLY | O_CREAT | O_EXCL, 0600);
 	if (fd == -1)
 	{
-		fd = open(filename, O_WRONLY | O_CREAT | O_EXCL, 0600);
+		fd = open(filename, O_WRONLY | O_TRUNC);
 		if (fd == -1)
 		{
 			return (-1);
