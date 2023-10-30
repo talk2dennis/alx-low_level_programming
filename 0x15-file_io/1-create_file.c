1d20a65d0233f2239a990be1588f1e3fb2f627b9
Dennis Adigwe <adigwedennis@gmail.com>
2023-10-30 20:03:54 +0100

    created task 2

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,7 +22,6 @@ int create_file(const char *filename, char *text_content)
 		{
 			return (-1);
 		}
-		return (1);
 	}
 	if (text_content != NULL || *text_content != '\0')
 	{
---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - function that appends to the end of a file
+ * @filename: the name of the file
+ * @text_content: the content to write
+ * Return: 1 on success otherwise -1
+ */
+
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd, fw;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+		return (-1);
+	if (text_content != NULL)
+	{
+		fw = write(file, text_content, strlen(text_content));
+		if (fw == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+		close(fd);
+		return (1);
+	}
+	else
+	{
+		close(fd);
+		return (1);
+	}
+}
