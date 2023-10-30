18d36c45b0669d5f02355f07b56bffba99a38df4
Dennis Adigwe <adigwedennis@gmail.com>
2023-10-31 00:06:05 +0100

    created append file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -20,7 +20,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 	if (text_content != NULL)
 	{
-		fw = write(file, text_content, strlen(text_content));
+		fw = write(fd, text_content, strlen(text_content));
 		if (fw == -1)
 		{
 			close(fd);
