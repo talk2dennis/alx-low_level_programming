6c40b6de0ae7a1d88d4a4e966d1003777f5ef31b
Dennis Adigwe <adigwedennis@gmail.com>
2023-10-30 14:54:19 +0100

    updated 0-read_textfile.c

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * read_textfile - A function that reads from a file and prints out its content
+ * @filename: the name of the file to read from
+ * @letters: number of characters to read from
+ * Return: returns the number of characters read or -1 on error
+ */
+
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t num, fd;
+	char buffer[1024];
+
+	if (filename == NULL)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	num = read(fd, buffer, letters);
+	if (num == -1)
+	{
+		close(fd);
+		return (0);
+	}
+	write(STDOUT_FILENO, buffer, num);
+	close(fd);
+	return (num);
+}
+
+
