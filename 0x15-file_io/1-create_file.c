781d328a6a0199dca0005eb884ffc3ca9f1f1300
Dennis Adigwe <adigwedennis@gmail.com>
2023-10-31 11:03:20 +0100

    updated 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,24 +14,20 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_WRONLY | O_CREAT | O_EXCL, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
 	{
-		fd = open(filename, O_WRONLY | O_TRUNC);
-		if (fd == -1)
-		{
-			return (-1);
-		}
+		return (-1);
 	}
-	if (text_content != NULL || *text_content != '\0')
+	if (text_content == NULL)
+		text_content = "";
+	fw = write(fd, text_content, strlen(text_content));
+	if (fw == -1)
 	{
-		fw = write(fd, text_content, strlen(text_content));
-		if (fw == -1)
-		{
-			close(fd);
-			return (-1);
-		}
+		close(fd);
+		return (-1);
 	}
+
 	close(fd);
 	return (1);
 }
