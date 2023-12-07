826a576825c90254f6d404dbc739601e6023d9b5
Dennis Adigwe <adigwedennis@gmail.com>
2023-12-07 13:23:50 +0100

    formated for betty

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -14,10 +14,10 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 
 	node = malloc(sizeof(dlistint_t));
 	if (node == NULL)
-		return NULL;
+		return (NULL);
 	node->n = n;
 	node->next = NULL;
-	
+
 	/*
 	 * if list is empty
 	 */
