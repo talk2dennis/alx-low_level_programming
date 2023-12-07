b87e647d210aece1e680e7ed23a5c695f5361632
Dennis Adigwe <adigwedennis@gmail.com>
2023-12-07 12:39:50 +0100

    completed task 2

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - A function that adds a node at the begining
+ * @head: pointer to the head
+ * @n: int data
+ * Return: returns pointer to list
+ */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *node;
+
+	node = malloc(sizeof(dlistint_t));
+	if (node == NULL)
+	{
+		printf("Malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
+
+	node->n = n;
+	node->next = *head;
+	node->prev = NULL;
+
+	if (*head != NULL)
+	{
+		(*head)->prev = node;
+	}
+	*head = node;
+	return (node);
+}
