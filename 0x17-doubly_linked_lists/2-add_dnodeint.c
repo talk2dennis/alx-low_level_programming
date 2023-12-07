3eb2b0fe50335e4c14fb72de1d0773d129f3a18f
Dennis Adigwe <adigwedennis@gmail.com>
2023-12-07 12:52:27 +0100

    updated task 2 to return null if mem fails

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -13,7 +13,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 
 	node = malloc(sizeof(dlistint_t));
 	if (node == NULL)
-		return (NULL)
+		return (NULL);
 	node->n = n;
 	node->next = *head;
 	node->prev = NULL;
