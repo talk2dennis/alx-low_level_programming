b28ed9797c5349bf0d56104af94db17e2e5075a5
Dennis Adigwe <adigwedennis@gmail.com>
2023-12-22 00:59:05 +0100

    used malloc instead calloc

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -16,7 +16,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 
 	htable->size = size;
-	htable->array = calloc(htable->size, sizeof(hash_node_t *));
+	htable->array = malloc(htable->size * sizeof(hash_node_t *));
 	if (htable->array == NULL)
 	{
 		free(htable);
