5779ab3990ee5a604e0208522cf488e85bb6b0fb
Dennis Adigwe <adigwedennis@gmail.com>
2023-12-22 06:59:05 +0100

    created a get function to retrieve data from hash table

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - A function that retrieves data using key from a hash table
+ * @ht: pointer to hash table
+ * @key: pointer to key
+ * Return: returns the value or null
+ */
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	hash_node_t *tmp;
+	unsigned long int index;
+
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (NULL);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	if (index >= ht->size)
+		return (NULL);
+	tmp = ht->array[index];
+	while (tmp)
+	{
+		if (strcmp(tmp->key, key) == 0)
+			return (tmp->value);
+		tmp = tmp->next;
+	}
+	return (NULL);
+}
