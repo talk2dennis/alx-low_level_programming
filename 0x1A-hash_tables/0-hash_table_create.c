3a6f90bf48405d6e0e5b438360dfbbb1110393a0
Dennis Adigwe <adigwedennis@gmail.com>
2023-12-22 01:07:06 +0100

    didn't free the memory alocated if memory fails

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -16,12 +16,10 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 
 	htable->size = size;
-	htable->array = malloc(htable->size * sizeof(hash_node_t *));
+	htable->array = malloc(sizeof(hash_node_t *) * size);
 	if (htable->array == NULL)
-	{
-		free(htable);
 		return (NULL);
-	}
+
 	for (i = 0; i < htable->size; i++)
 	{
 		htable->array[i] = NULL;
