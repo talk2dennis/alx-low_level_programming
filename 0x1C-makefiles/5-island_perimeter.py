0d8d44045beab2125fce9107f6f91cef491d3ddc
Dennis Adigwe <adigwedennis@gmail.com>
2024-02-21 18:13:01 +0100

    completed 5-island_perimeter

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+island_perimeter module
+"""
+
+
+def island_perimeter(grid):
+    """ island_perimeter - returns the perimeter of the
+                            island described in grid
+    Args:
+        grid (list): a list of list of integers
+    Return: the perimeter of the island
+    """
+    height = len(grid)
+    width = len(grid[0])
+    size = 0
+    side = 0
+
+    for row in range(len(grid)):
+        for col in range(len(grid[0])):
+            if grid[row][col] == 1:
+                size += 1
+                if grid[row - 1][col] == 1 or grid[row][col - 1] == 1:
+                    side += 1
+    return (size * 4) - (side * 2)
